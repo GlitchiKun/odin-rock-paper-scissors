1d7e5567b4f24a282a1d63198ab5ec9893ad6698
Charlie Poorun <p.charlie17@gmail.com>
2026-04-16 22:44:03 +0200

    feat: add the ability to play a round

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function getComputerChoice() {
   choice = Math.random() * 3;
 
   if (choice < 1) {
-    return "Rock";
+    return "ROCK";
   } else if (choice < 2) {
-    return "Paper";
+    return "PAPER";
   } else {
-    return "Scissors";
+    return "SCISSORS";
   }
 }
 
@@ -17,4 +17,26 @@ function getHumainChoice() {
   return prompt("Rock, Paper or Scissors ?");
 }
 
-console.log(getHumainChoice());
+function playRound(computerChoice, humanChoice) {
+  humanChoice = humanChoice.toUpperCase();
+
+  if (humanChoice == computerChoice) {
+    console.log(`Draw ! Computer also choose ${computerChoice} !`);
+    return;
+  }
+
+  const isHumanWin =
+    (humanChoice == "ROCK" && computerChoice == "SCISSORS") ||
+    (humanChoice == "PAPER" && computerChoice == "ROCK") ||
+    (humanChoice == "SCISSORS" && computerChoice == "PAPER");
+
+  if (isHumanWin) {
+    console.log(`You win ! ${humanChoice} beats ${computerChoice}.`);
+    humainScore++;
+  } else {
+    console.log(`You loose ! ${computerChoice} beats ${humanChoice}.`);
+    computerScore++;
+  }
+}
+
+playRound("PAPER", "ROCK");
